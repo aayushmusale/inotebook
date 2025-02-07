6a42539017569f5c95cc5c9c35a1bd10bd234e28
aayushmusale <aayushmusale05@gmail.com>
2025-02-07 21:21:32 +0530

    Second commit

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,7 +6,7 @@ const About = () => {
   return (
     <div className="container my-4">
      <h4>About me:</h4>
-     <p>My name is <b>Aayush:The Great</b><br/> I am a very good man, rest all are <b>YEDZAVE</b></p>
+     <p>My name is <b>Aayush:The Great</b><br/></p>
     </div>
   )
 }
